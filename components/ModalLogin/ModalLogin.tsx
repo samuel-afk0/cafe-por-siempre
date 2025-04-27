9ef99652c70d898bcfc3bbe4b087f32d1aa028cb
Samuel <134749814+samuel-afk0@users.noreply.github.com>
2025-04-27 12:40:51 -0600

    s

--- components/ModalLogin/ModalLogin.tsx
+++ components/ModalLogin/ModalLogin.tsx
@@ -37,14 +37,14 @@ const ModalLogin = ({ isOpen, onClose }: ModalLoginProps) => {
     }, 300);
   };
 
-  // Lógica para manejar el inicio de sesión con Instagram
-  const handleInstagramLogin = () => {
-    const clientId = "1709742513239481"; // Asegúrate de reemplazarlo con tu ID de cliente real.
-    const redirectUri = "TU_REDIRECT_URI"; // El URI al que Instagram redirigirá después de la autenticación.
-    const instagramAuthUrl = `https://api.instagram.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=user_profile,user_media&response_type=code`;
+  // Lógica para manejar el inicio de sesión con Facebook
+  const handleFacebookLogin = () => {
+    const appId = "TU_APP_ID";  // Reemplaza con tu ID de aplicación de Facebook
+    const redirectUri = "TU_REDIRECT_URI"; // El URI al que Facebook redirigirá después de la autenticación.
+    const facebookAuthUrl = `https://www.facebook.com/v12.0/dialog/oauth?client_id=${appId}&redirect_uri=${redirectUri}&scope=email,public_profile&response_type=code`;
 
-    // Abre la URL de autorización de Instagram en una nueva ventana o navegador
-    window.location.href = instagramAuthUrl;
+    // Abre la URL de autorización de Facebook en una nueva ventana o navegador
+    window.location.href = facebookAuthUrl;
   };
 
   if (animationState === 'exited') return null;
@@ -115,13 +115,13 @@ const ModalLogin = ({ isOpen, onClose }: ModalLoginProps) => {
           
           <button
             type="button"
-            onClick={handleInstagramLogin}
-            className="w-full flex items-center justify-center space-x-2 bg-gradient-to-r from-purple-500 via-pink-500 to-orange-500 text-white py-2 px-4 rounded-md hover:opacity-90 transition-all duration-300 hover:shadow-lg hover:shadow-pink-500/20"
+            onClick={handleFacebookLogin}
+            className="w-full flex items-center justify-center space-x-2 bg-gradient-to-r from-blue-600 via-blue-700 to-blue-800 text-white py-2 px-4 rounded-md hover:opacity-90 transition-all duration-300 hover:shadow-lg hover:shadow-blue-500/20"
           >
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
-              <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zM12 0C8.741 0 8.333.014 7.053.072 2.695.272.273 2.69.073 7.052.014 8.333 0 8.741 0 12c0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98C8.333 23.986 8.741 24 12 24c3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98C15.668.014 15.259 0 12 0zm0 5.838a6.162 6.162 0 100 12.324 6.162 6.162 0 000-12.324zM12 16a4 4 0 110-8 4 4 0 010 8zm6.406-11.845a1.44 1.44 0 100 2.881 1.44 1.44 0 000-2.881z"/>
+              <path d="M22 12c0 5.5-4.5 10-10 10S2 17.5 2 12 6.5 2 12 2s10 4.5 10 10zM12 4c-4.4 0-8 3.6-8 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8zm.7 11h-1.4v-3.3h1.4V15zm0-4.6h-1.4V7.6h1.4v3.8z" />
             </svg>
-            <span>Iniciar Sesión con Instagram</span>
+            <span>Iniciar Sesión con Facebook</span>
           </button>
         </form>
       </div>
